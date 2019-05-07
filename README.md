33c83193dae51e6c4341f79104dc0953a68db044
Howy <Howy@Howys-MBP.local>
2019-05-07 00:06:35 -0400

    improved logic, tested with 4 test cases

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# codingtest_coinfund
+## FIFO Cryptocurrency Trades Calculator
+## Overview
+This implements a cryptocurrency trades calculator using FIFO order. The program prints current holdings and a profit/loss breakdown for each cryptocurrency purchased.
+Coinfund.io's coding assignment for Summer 2019 Intern Applications.
+
+## Running the program
+python3 fifo.py transactions.csv
+
+## Tests
+Tested with 4 test cases: test.csv, test2.csv, test3.csv and test4.csv.
+test.csv: Basic test case
+test2.csv: Test for overflows
+test3.csv: Testing FIFO order
+test4.csv: Testing across multiple coins
+
+## Authors
+Howy Ho
--- fifo.py
+++ fifo.py
@@ -1,5 +1,7 @@
 import csv
+import sys
 import collections
+from decimal import *
 
 # assumption1: dates given in chronological order
 
@@ -13,46 +15,72 @@ class Asset:
         self.q = collections.deque()
         self.name = name
 
-    def transact(self, price, amount):
-        balance = 0
-        if (amount > 0):
-            self.q.append(Transaction(price, amount))
-            return balance
+    def buy(self, price, amount):
+        self.q.append(Transaction(price, amount))
 
+    def sell(self, price, amount):
+        balance = 0
         #can probably improve logic here for runtime
         while (amount < 0):
             if self.q:
-                self.q.append(Transaction(price, 0))
-                if (amount > self.q[0].amount):
+                if (-amount > self.q[0].amount):
                     removedtx = self.q.popleft()
                     balance += removedtx.amount * (price - removedtx.price)
-                    amount -= removedtx.amount
+                    amount += removedtx.amount
                 else:
                     #signs flipped to account for negative amount
                     self.q[0].amount += amount
                     balance -= amount*(price - self.q[0].price)
+                    self.q.append(Transaction(price, 0))
                     return balance
             else:
                 print("Error: detected sale before purchase (short selling is not supported)")
-                return 0
+                exit()
 
-#change so receives filetype
-with open('test.csv') as csv_file:
+with open(sys.argv[1], 'r') as csv_file:
     thisdict = {}
-    pnl = 0
+    pnldict = {}
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_number_debug = 0
+    totalpnl = 0
+    totalassets = 0
     next(csv_reader)
     for row in csv_reader:
         line_number_debug += 1
         if (row[1] not in thisdict):
             thisdict[row[1]] = Asset(row[1])
-        pnl += thisdict[row[1]].transact(int(row[2]), int(row[3]))
+        if (Decimal(row[3]) > 0):
+            thisdict[row[1]].buy(Decimal(row[2]), Decimal(row[3]))
+        else:
+            if (row[1] not in pnldict):
+                pnldict[row[1]] = 0
+            pnldict[row[1]] += thisdict[row[1]].sell(Decimal(row[2]), Decimal(row[3]))
 
-print("pnl is:", pnl)
-#portfolio value
+#Printing Portfolio Value
+for x in thisdict:
+    for y in thisdict[x].q:
+        total_amount = 0
+        for y in thisdict[x].q:
+            total_amount += y.amount
+    if (total_amount != 0):
+        totalassets+=1
+print("Portfolio: ({0} assets)".format(totalassets))
 for x in thisdict:
     total_amount = 0
     for y in thisdict[x].q:
         total_amount += y.amount
-    print(thisdict[x].name, total_amount, thisdict[x].q[-1].price)
+    if (total_amount != 0):
+        print("{0}: {1} ${2}".format(thisdict[x].name, total_amount, total_amount*thisdict[x].q[-1].price))
+
+#Printing PnL
+print("Portfolio: ({0} assets)".format(len(pnldict)))
+for x in pnldict:
+    neg = ""
+    if (pnldict[x] < 0):
+        neg = "-"
+    print("{2}: {1}${0}".format(abs(pnldict[x]),neg, x))
+    totalpnl += pnldict[x]
+neg = ""
+if (totalpnl < 0):
+    neg = "-"
+print("Total P&L: {1}${0}".format(abs(totalpnl),neg))
