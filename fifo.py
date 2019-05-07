e77ac9dc8df921464f86a1fee45b73fcfffa60de
Howy <Howy@Howys-MBP.local>
2019-05-07 02:05:31 -0400

    cleaned up syntax, improved formatting

--- fifo.py
+++ fifo.py
@@ -3,8 +3,6 @@ import sys
 import collections
 from decimal import *
 
-# assumption1: dates given in chronological order
-
 class Transaction:
     def __init__(self, price, amount):
         self.price = price
@@ -23,12 +21,12 @@ class Asset:
         #can probably improve logic here for runtime
         while (amount < 0):
             if self.q:
+                #All signs flipped to account for negative amount
                 if (-amount > self.q[0].amount):
                     removedtx = self.q.popleft()
                     balance += removedtx.amount * (price - removedtx.price)
                     amount += removedtx.amount
                 else:
-                    #signs flipped to account for negative amount
                     self.q[0].amount += amount
                     balance -= amount*(price - self.q[0].price)
                     self.q.append(Transaction(price, 0))
@@ -37,50 +35,55 @@ class Asset:
                 print("Error: detected sale before purchase (short selling is not supported)")
                 exit()
 
-with open(sys.argv[1], 'r') as csv_file:
-    thisdict = {}
-    pnldict = {}
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_number_debug = 0
-    totalpnl = 0
-    totalassets = 0
-    next(csv_reader)
-    for row in csv_reader:
-        line_number_debug += 1
-        if (row[1] not in thisdict):
-            thisdict[row[1]] = Asset(row[1])
-        if (Decimal(row[3]) > 0):
-            thisdict[row[1]].buy(Decimal(row[2]), Decimal(row[3]))
-        else:
-            if (row[1] not in pnldict):
-                pnldict[row[1]] = 0
-            pnldict[row[1]] += thisdict[row[1]].sell(Decimal(row[2]), Decimal(row[3]))
+if __name__ == '__main__':
+    with open(sys.argv[1], 'r') as csv_file:
+        thisdict = {}
+        pnldict = {}
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        next(csv_reader)
+        for row in csv_reader:
+            if (row[1] not in thisdict):
+                thisdict[row[1]] = Asset(row[1])
+            if (Decimal(row[3]) > 0):
+                thisdict[row[1]].buy(Decimal(row[2]), Decimal(row[3]))
+            else:
+                if (row[1] not in pnldict):
+                    pnldict[row[1]] = 0
+                pnldict[row[1]] += thisdict[row[1]].sell(Decimal(row[2]), Decimal(row[3]))
 
-#Printing Portfolio Value
-for x in thisdict:
-    for y in thisdict[x].q:
+    #Printing Portfolio Value
+    totalassets = 0
+    totalpnl = 0
+    for x in thisdict:
+        for y in thisdict[x].q:
+            total_amount = 0
+            for y in thisdict[x].q:
+                total_amount += y.amount
+        if (total_amount != 0):
+            totalassets+=1
+    plural = ""
+    if (totalassets > 1):
+        plural = "s"
+    print("Portfolio: ({0} asset{1})".format(totalassets, plural))
+    for x in thisdict:
         total_amount = 0
         for y in thisdict[x].q:
             total_amount += y.amount
-    if (total_amount != 0):
-        totalassets+=1
-print("Portfolio: ({0} assets)".format(totalassets))
-for x in thisdict:
-    total_amount = 0
-    for y in thisdict[x].q:
-        total_amount += y.amount
-    if (total_amount != 0):
-        print("{0}: {1} ${2}".format(thisdict[x].name, total_amount, total_amount*thisdict[x].q[-1].price))
+        if (total_amount != 0):
+            print("{0}: {1} ${2}".format(thisdict[x].name, total_amount, total_amount*thisdict[x].q[-1].price))
 
-#Printing PnL
-print("Portfolio: ({0} assets)".format(len(pnldict)))
-for x in pnldict:
+    plural = ""
+    if (len(pnldict) > 1):
+        plural = "s"
+    #Printing PnL
+    print("Portfolio: ({0} asset{1})".format(len(pnldict), plural))
+    for x in pnldict:
+        neg = ""
+        if (pnldict[x] < 0):
+            neg = "-"
+        print("{2}: {1}${0}".format(abs(pnldict[x]),neg, x))
+        totalpnl += pnldict[x]
     neg = ""
-    if (pnldict[x] < 0):
+    if (totalpnl < 0):
         neg = "-"
-    print("{2}: {1}${0}".format(abs(pnldict[x]),neg, x))
-    totalpnl += pnldict[x]
-neg = ""
-if (totalpnl < 0):
-    neg = "-"
-print("Total P&L: {1}${0}".format(abs(totalpnl),neg))
+    print("Total P&L: {1}${0}".format(abs(totalpnl),neg))
